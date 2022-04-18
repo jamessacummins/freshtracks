5deda5e987e42b88d61e27162af8fcb533ddebb7
James Cummins <james.cummins@renewtrak.com>
2022-04-18 19:21:56 +1000

    Fixing up page issues

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">-->
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
 
   <link rel="stylesheet" href="css/main.css">
   <link rel="stylesheet" href="css/home.css">
@@ -107,24 +107,9 @@
         </div>
 
       </div>
-</div>
+    </div>
 
-<?php include 'footer.php'; ?>
-
-
-
-  </div>
-
-  </div>
-  </div>
-  </div>
-  </nav>
-
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min,js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.boostrapcdn.com/boostrap/4.5.2/js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script>
+    <?php include 'footer.php'; ?>
 
 </body>
 
