dc9700ae03539bb1861393dc743db7e615e16136
James Cummins <james.cummins@renewtrak.com>
2022-04-30 21:11:49 +1000

    Adding basic outdoor content

--- outdoor.php
+++ outdoor.php
@@ -13,10 +13,61 @@
 
         <?php include "nav.php" ?>
 
-        <div class="parent-topbar-container" id="abi-topbar-parent">
+        <div class="parent-topbar-container" id="outdoor-topbar-parent">
             <div class="topbar-overlay"></div>
             <div class="topbar-container">
-                TBC
+                Outdoor cycling
+            </div>
+        </div>
+
+        <div class="section-container" id="why">
+            <div class="section-background-overlay"></div>
+            <div class="section">
+                <div class="section-subheading">Why cycle hubs?</div>
+                <div class="section-text">
+                    Riding on recumbent bikes builds strength, stimulates the brain and importantly provides a chance to meet with others.
+                </div>
+            </div>
+        </div>
+        <div class="section-container" id="how">
+            <div class="section-background-overlay"></div>
+            <div class="section">
+                <img class="arrow-leftofscreen">
+                <div class="section-subheading">How does it work?</div>
+                <div class="section-text">
+                    Riders are first assessed by specialists from Royal Rehab Hospital. Once accessed riders can join our supervised group riding sessions. All recumbent bikes are provided on site.
+                </div>
+            </div>
+        </div>
+        <div class="section-container" id="benefits">
+            <div class="section-background-overlay"></div>
+            <div class="section">
+                <img class="arrow-rightofscreen">
+                <div class="section-subheading">What are the benefits?</div>
+                <div class="section-text">
+                    Riding a recumbent bike can be a great form of exercise but it is far more than this for people with an acquired brain injury. As well as being fun, using the bikes helps:
+                    <br>
+                    <br>
+                    <li>Improve strength and endurance</li>
+                    <li> Cognitive development through spatial awareness
+                    </li>
+                    <li> Cognitive development through spatial awareness
+                    </li>
+                    <li> Socially by providing opportunities to meet and build relationships with others.
+                    </li>
+                    <li> The Hubs can also provide a meaningful pursuit where paid employment is not a possibility.
+                    </li>
+                </div>
+            </div>
+        </div>
+        <div class="section-container" id="join">
+            <div class="section-background-overlay"></div>
+            <div class="section">
+                <img class="arrow-leftofscreen">
+                <div class="section-subheading">How can I join?</div>
+                <div class="section-text">
+                To register for an assessment: call Kylie on 0478488568 , 02 9808 9369 or email her on Kylie.chambers@royalrehab.com.au
+                </div>
             </div>
         </div>
 
