32cd50a9b5df8b0d73e9e3ee0e2aaa6e34dd3178
Chocycrumble <53751552+Chocycrumble@users.noreply.github.com>
2022-05-04 12:53:16 +1000

    Added Steve

---
+++ steve.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "head.php" ?>
+    <link rel="stylesheet" href="./css/steve.css">
+    <!-- test  -->
+</head>
+
+
+<body>
+
+    <div class="main">
+
+        <?php include "nav.php" ?>
+
+        <div class="parent-topbar-container" id="abi-topbar-parent">
+            <div class="topbar-overlay"></div>
+            <div class="topbar-container">
+                Strength
+            </div>
+        </div>
+
+
+        <div class="section-container" id="what-is-an-abi">
+            <div class="section">
+                <div class="section-subheading">Steve’s Story</div>
+                <div class="section-text">
+                Steve is a very charismatic personality, that everybody loves and enjoys 
+                to share their time with. Every winter on saturdays this was Steve’s one day 
+                of the week where he did the 1 thing he loved the most – playing rugby with 
+                his mates. Saturday 28 Apr 2012, was  no different, a normal day of rugby!
+                 Steve came off the field taking a knock to his nose that rattled him enough 
+                 to come off for a rest. Steve did go back out and play feeling ok within himself. 
+                 Playing for over 13yrs Steve knew his mental and physical limits within this sport.
+                    <br><br>
+                    Later that night Steve would go out with friends and family to celebrate what
+                     would be his 25th bday the very next day, Sunday 29 April. The evening was a 
+                     fun and energetic time bouncing from local bars to dance floors and eventually
+                      making it home after a long night to continue his festivities with a handful 
+                      of family members. As night rolled into morning and Steve realised his ability
+                       to drink anymore was coming to an end, he took himself off to the bathroom where 
+                       he would later be found passed out on the tiles. Awoken by his sister who noticed
+                        he was incoherent (assumed by too many bday drinks) gave him plenty of water to drink
+                         before moving him to the comfort of the couch to rest off his evening.
+                    <br><br>
+                    Sadly Steve didn’t wake up again on his bday Sunday. Steve had vomited in his
+                     sleep closing off his airways. He suffocated his brain and lungs of oxygen by 
+                     obstruction to his airways. Although his family did give him 1st Aid, he was revived 
+                     by paramedics who stabilised him to get him to hospital, where he was put in an induced coma for 3 weeks.
+                    <br><br>
+                    In this time his family learnt from Neurologists, ICU doctors and specialists that 
+                    a number of factors in Steves ‘normal’ saturday could have lead to the severity of his accident.
+                     A possible concussion from the knock to his nose at rugby and dehydration – caused by not enough 
+                     water and electrolytes in his body at rugby and by alcohol later in the night, meant that Steve should
+                      not have been out celebrating his bday. These 2 things combined required rest and stability. 
+                      A later recognisable bump to his head believed to have been acquired from his fall in the bathroom 
+                      could have caused further damage and concussion. Hence by the time he was laid to rest his brain 
+                      and body were not able to respond to his bodies need to clear his airways properly.
+                    <br><br>
+                    Unfortunately no-one can be certain how long Steve’s brain was starved of oxygen. Brain
+                     injuries happen to many people of all ages, in different ways. No single brain injury is the 
+                     same as the next. Everyones recovery and their capacity to recover is very individual. Since awaking 
+                     from his coma, recovery has been a long adventure of highs, lows, quick fixes, delays, time, 
+                     energy, care and mostly support. From the ICU ward of Manly hospital, to Royal ryde rehab clinic and
+                      moving home the need to give Steve the right age appropriate care, future living conditions and support
+                       is paramount for our family. Therapy on a weekly basis consists of fitness training, speech therapy, 
+                       computer brain training, occupational therapy and functional neurology. The support of annual private 
+                       fundraising events and places like FreshTracks give Steve and his family hope that one day he may be able 
+                       to live an independent and youthful life for himself again.
+                    <br><br>
+                    The charismatic personality of Steve that everyone loves shines on a daily basis. We are grateful 
+                    for Freshtracks and their efforts to give back to our brothers, sisters, daughters, sons, parents who are on their recovery journey.
+                    <br><br>
+                    From Steve’s biggest fan, his sister Kelly x
+
+                </div>
+            </div>
+        </div>
+
+
+
+        <?php include 'footer.php'; ?>
+
+    </div>
+
+    </div>
+
+</body>
+
+</html>
